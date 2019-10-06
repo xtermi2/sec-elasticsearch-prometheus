16ce53835595966df8031da37f68610216bdfa8f
Andreas Keefer <keefer.a@gmail.com>
2019-10-06 11:16:36 +0200

    better test resilience for travis

--- src/test/test_case_cluster_health.sh
+++ src/test/test_case_cluster_health.sh
@@ -8,7 +8,7 @@ echo -n "TEST if elasticsearch cluster state is green..."
 status=""
 health=""
 itterations=0
-while [[ "${status,,}" != "green" && $itterations -lt 60 ]]; do
+while [[ "${status,,}" != "green" && $itterations -lt 30 ]]; do
   sleep 1
   health=$(curl -X GET --silent -k -u "elastic:elastic" "https://localhost:9200/_cluster/health")
   status=$(jq -r .status <<<"${health}")
@@ -23,16 +23,25 @@ else
 fi
 
 echo -n "TEST if elasticsearch cluster has 2 nodes..."
-number_of_nodes=$(jq -r .number_of_nodes <<<"${health}")
-if [ "${number_of_nodes,,}" != "2" ]; then
-  echo "failed: number_of_nodes is ${number_of_nodes}"
+number_of_nodes=""
+health=""
+itterations=0
+while [[ "${number_of_nodes}" != "2" && $itterations -lt 30 ]]; do
+  sleep 1
+  health=$(curl -X GET --silent -k -u "elastic:elastic" "https://localhost:9200/_cluster/health")
+  number_of_nodes=$(jq -r .number_of_nodes <<<"${health}")
+  echo -n "."
+  ((itterations++))
+done
+if [ "${number_of_nodes}" != "2" ]; then
+  echo "failed: number_of_nodes is \"${number_of_nodes}\""
   ((general_status++))
 else
   echo "OK"
 fi
 number_of_data_nodes=$(jq -r .number_of_data_nodes <<<"${health}")
 if [ "${number_of_data_nodes,,}" != "2" ]; then
-  echo "failed: number_of_data_nodes is ${number_of_data_nodes}"
+  echo "failed: number_of_data_nodes is \"${number_of_data_nodes}\""
   ((general_status++))
 else
   echo "OK"
