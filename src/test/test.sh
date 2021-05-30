8fb1f0d9c491e98151283b64940f6c7d6e96627d
Andreas Keefer <xtermi2@users.noreply.github.com>
2021-05-30 09:56:27 +0200

    first pull images, to get a more reliable build in CI

--- src/test/test.sh
+++ src/test/test.sh
@@ -4,6 +4,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "${DIR}"
 
 echo "stating docker-compose"
+# first pull images, to get a more reliable build in CI
+docker-compose pull kibana filebeat
 docker-compose up -d
 
 ./wait_until_started.sh
